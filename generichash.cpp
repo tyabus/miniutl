4611f298ff5aa3c6f03900b9116ba391afa46c2d
Alibek Omarov <a1ba.omarov@gmail.com>
2019-12-23 06:26:21 +0300

    generichash: comment fallthrough

--- generichash.cpp
+++ generichash.cpp
@@ -78,8 +78,8 @@ uint32 MurmurHash3_32( const void * key, size_t len, uint32 seed, bool bCaseless
 
 	switch(len & 3)
 	{
-	case 3: k1 ^= tail[2] << 16;
-	case 2: k1 ^= tail[1] << 8;
+	case 3: k1 ^= tail[2] << 16; // fallthrough
+	case 2: k1 ^= tail[1] << 8; // fallthrough
 	case 1: k1 ^= tail[0];
 			k1 &= uSourceBitwiseAndMask;
 			k1 *= 0xcc9e2d51;
@@ -153,23 +153,23 @@ void MurmurHash3_128( const void * key, const int len, const uint32 seed, void *
 
 	switch ( len & 15 )
 	{
-	case 15: k2 ^= ( ( uint64 )tail[14] ) << 48;
-	case 14: k2 ^= ( ( uint64 )tail[13] ) << 40;
-	case 13: k2 ^= ( ( uint64 )tail[12] ) << 32;
-	case 12: k2 ^= ( ( uint64 )tail[11] ) << 24;
-	case 11: k2 ^= ( ( uint64 )tail[10] ) << 16;
-	case 10: k2 ^= ( ( uint64 )tail[9] ) << 8;
-	case  9: k2 ^= ( ( uint64 )tail[8] ) << 0;
+	case 15: k2 ^= ( ( uint64 )tail[14] ) << 48; // fallthrough
+	case 14: k2 ^= ( ( uint64 )tail[13] ) << 40; // fallthrough
+	case 13: k2 ^= ( ( uint64 )tail[12] ) << 32; // fallthrough
+	case 12: k2 ^= ( ( uint64 )tail[11] ) << 24; // fallthrough
+	case 11: k2 ^= ( ( uint64 )tail[10] ) << 16; // fallthrough
+	case 10: k2 ^= ( ( uint64 )tail[9] ) << 8; // fallthrough
+	case  9: k2 ^= ( ( uint64 )tail[8] ) << 0; // fallthrough
 		k2 *= c2; k2 = ROTL64( k2, 33 ); k2 *= c1; h2 ^= k2;
-
-	case  8: k1 ^= ( ( uint64 )tail[7] ) << 56;
-	case  7: k1 ^= ( ( uint64 )tail[6] ) << 48;
-	case  6: k1 ^= ( ( uint64 )tail[5] ) << 40;
-	case  5: k1 ^= ( ( uint64 )tail[4] ) << 32;
-	case  4: k1 ^= ( ( uint64 )tail[3] ) << 24;
-	case  3: k1 ^= ( ( uint64 )tail[2] ) << 16;
-	case  2: k1 ^= ( ( uint64 )tail[1] ) << 8;
-	case  1: k1 ^= ( ( uint64 )tail[0] ) << 0;
+		// fallthrough
+	case  8: k1 ^= ( ( uint64 )tail[7] ) << 56; // fallthrough
+	case  7: k1 ^= ( ( uint64 )tail[6] ) << 48; // fallthrough
+	case  6: k1 ^= ( ( uint64 )tail[5] ) << 40; // fallthrough
+	case  5: k1 ^= ( ( uint64 )tail[4] ) << 32; // fallthrough
+	case  4: k1 ^= ( ( uint64 )tail[3] ) << 24; // fallthrough
+	case  3: k1 ^= ( ( uint64 )tail[2] ) << 16; // fallthrough
+	case  2: k1 ^= ( ( uint64 )tail[1] ) << 8; // fallthrough
+	case  1: k1 ^= ( ( uint64 )tail[0] ) << 0; // fallthrough
 		k1 *= c1; k1 = ROTL64( k1, 31 ); k1 *= c2; h1 ^= k1;
 	};
 
